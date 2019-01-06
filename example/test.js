3adb37e66be6ed1802deb22553aef6bbb51cc190
BanSky <tufeigd@163.com>
2019-01-06 21:09:18 +0800

    添加新的解释

--- example/test.js
+++ example/test.js
@@ -1,79 +1,34 @@
-const sprotobuf = require("../lib")
-
 const proto = `
-message error
-{
-    int32 code = 1
-    string reason = 2
-}
-message call
-{
-    uint32 service = 1
-    uint32 method = 2
-    uint32 session = 3
-    bytes req = 4
-}
-message response
-{
-    uint32 session = 1
-    error err = 2
-    bytes resp = 3
-}
-message invoke
-{
-    uint32 service = 1
-    uint32 method = 2
-    bytes req = 3
-}
-message net_data
-{
-    call ca = 1
-    invoke inv = 2
-    response resp = 3
-}
-
 message example_test
 {
+    int32 int_val = 1
+    string string_val = 2
+    bool bool_val = 3
+    double double_val = 4
     [int32] int_vals = 5
     <string,int32> map_val = 6
 }
-
-service example
-{
-    option id = 1
-    rpc test (void) returns (mbool)
-    {
-        option id = 1
-    }
-    rpc echo(mstring) returns(mstring)
-    {
-        option id = 2
-    }
-}
-
-service chat
-{
-    option id = 2
-    rpc speak(mstring) returns(mbool)
-    {
-        option id = 1
-    }
-}
-
-message mstring
-{
-    string val = 1
-}
-
-message mbool
-{
-    bool val = 1
-}
-
-message void
-{
-
-}
 `
+const sprotobuf = require("../lib")
+const op = require("buffer-op")
 
-sprotobuf.parse(proto)
+const root = sprotobuf.parse(proto)
+
+let obj = {
+    int_val: 1,
+    string_val: "123",
+    bool_val: false,
+    double_val: 1.2345,
+    int_vals: [1, 2, 3],
+    map_val: new Map([["123", 123], ["234", 234], ["4234", 4234]])
+}
+
+let stream = new op.Stream()
+let writer = new op.Writer(stream)
+let reader = new op.Reader(stream)
+
+sprotobuf.to_buf(root, writer, "example_test", obj)
+
+let resp = sprotobuf.from_buf(root, reader, "example_test")
+
+console.dir(resp, 10)
